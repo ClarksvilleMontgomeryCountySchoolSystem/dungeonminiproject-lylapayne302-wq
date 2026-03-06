bee4a9d7432680cda68532f99986f1f13c49a9e4
lylapayne302-wq <lyla.payne302@students.cmcss.net>
2026-03-06 12:20:42 -0600

    the

--- task2.py
+++ task2.py
@@ -1,3 +1,29 @@
+bad = r"""
+                        888                      
+                        888                      
+                        888                      
+ 8888b. 88888b. 888  888888  888 .d88b. 888  888 
+    "88b888 "88b888  888888 .88Pd8P  Y8b888  888 
+.d888888888  888888  888888888K 88888888888  888 
+888  888888  888Y88b 888888 "88bY8b.    Y88b 888 
+"Y888888888  888 "Y88888888  888 "Y8888  "Y88888 
+                     888                     888 
+                Y8b d88P                Y8b d88P 
+                 "Y88P"                  "Y88P" 
+"""
+good = r"""
+
+                                   88                               
+                                   88                               
+                                   88                               
+,adPPYYba, 8b,dPPYba,  8b       d8 88   ,d8  ,adPPYba, 8b       d8  
+""     `Y8 88P'   `"8a `8b     d8' 88 ,a8"  a8P_____88 `8b     d8'  
+,adPPPPP88 88       88  `8b   d8'  8888[    8PP"""""""  `8b   d8'   
+88,    ,88 88       88   `8b,d8'   88`"Yba, "8b,   ,aa   `8b,d8'    
+`"8bbdP"Y8 88       88     Y88'    88   `Y8a `"Ybbd8"'     Y88'     
+                           d8'                             d8'      
+                          d8'                             d8'    
+"""
 has_key = False
 if not has_key:
     outcome = "Doom: We have failed our mission."
--- task3.py
+++ task3.py
@@ -1,3 +1,24 @@
+good = r"""
+     _               _               
+    | |             | |              
+ ___| |__   __ _  __| | _____      __
+/ __| '_ \ / _` |/ _` |/ _ \ \ /\ / /
+\__ \ | | | (_| | (_| | (_) \ V  V / 
+|___/_| |_|\__,_|\__,_|\___/ \_/\_/ 
+"""
+bad = r"""
+                                                           
+88                              88                         
+88                              88                         
+88                              88                         
+88,dPPYba,  ,adPPYYba,  ,adPPYb,88  ,adPPYb,d8  ,adPPYba,  
+88P'    "8a ""     `Y8 a8"    `Y88 a8"    `Y88 a8P_____88  
+88       d8 ,adPPPPP88 8b       88 8b       88 8PP"""""""  
+88b,   ,a8" 88,    ,88 "8a,   ,d88 "8a,   ,d88 "8b,   ,aa  
+8Y"Ybbd8"'  `"8bbdP"Y8  `"8bbdP"Y8  `"YbbdP"Y8  `"Ybbd8"'  
+                                    aa,    ,88             
+                                     "Y8bbdP"   
+"""
 guard_awake = True
 if guard_awake:
     outcome = "Doom: OH NO RUN!"
